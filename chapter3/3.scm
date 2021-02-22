63091849c75be6fe2aa8a6add6f185e0eb5672b3
Udeshya Dhungana <075BCT095.udeshya@pcampus.edu.np>
2021-02-22 20:43:30 +0545

    did monte carlo

--- chapter3/3.scm
+++ chapter3/3.scm
@@ -1,3 +1,25 @@
 
 
 ;; Tomorrow
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds"))
+
+  (define (deposit amount)
+    (set! balance (+ balance amount)))
+
+  (define (incorrect amount)
+    "Incorrect password")
+
+  (define (dispatch passwd m)
+    (if (eq? passwd password)
+	(cond ((eq? m 'withdraw) withdraw)
+	      ((eq? m 'deposit) deposit)
+	      (else (error "Unavilable function -- MAKE-ACCOUNT"
+			   m)))
+	incorrect))
+  dispatch)
---
+++ chapter3/6.scm
@@ -0,0 +1,17 @@
+
+;; Resetting the random number
+
+(define rand
+  (let ((x random-init))
+    (define (dispatch message)
+      ;; on getting symbol 'generate, we return a value
+      (cond ((eq? message 'generate)
+	     (begin (set! x (rand-update x))
+		    x))
+	    ((eq? message 'reset)
+	     ;; on gettting 'reset, we return a function 
+	     (lambda (value)
+	       (set! x value)))))
+    dispatch))
+
+;; rand is a variable that represents a function
