3c0acdf4d44e7faa83640b1f4f216d82a7f13a92
Udeshya Dhungana <075BCT095.udeshya@pcampus.edu.np>
2021-02-24 22:07:51 +0545

    started last exercise of section 1

---
+++ chapter3/7.scm
@@ -0,0 +1,44 @@
+
+
+
+;; My solution involves making a list of passwords
+;; On calling make-joint, add-pw procedure is called
+;; which adds this new password onto the list of passwords
+(define (make-account balance password)
+  (let ((passwords (list password)))
+
+    (define (match-passwd? passwd passwords)
+      (cond ((null? passwords) false)
+	    ((eq? (car passwords) passwd) true)
+	    (else (match-passwd? passwd (cdr passwords)))))
+    
+    (define (withdraw amount)
+      (if (>= balance amount)
+	  (begin (set! balance (- balance amount))
+		 balance)
+	  "Insufficient funds"))
+
+    (define (deposit amount)
+      (begin (set! balance (+ balance amount))
+	     balance))
+
+    (define (incorrect amount)
+      "Incorrect password")
+
+    (define (add-pw new-pw)
+      (begin (set! passwords (cons new-pw passwords))
+	     dispatch))
+
+    (define (dispatch passwd m)
+      (if (match-passwd? passwd passwords)
+	  (cond ((eq? m 'withdraw) withdraw)
+		((eq? m 'add-pw) add-pw)
+		((eq? m 'deposit) deposit)
+		(else (error "Unavilable function -- MAKE-ACCOUNT"
+			     m)))
+	  incorrect))
+    
+    dispatch))
+
+(define (make-joint orig-acc orig-passwd new-passwd)
+  ((orig-acc orig-passwd 'add-pw) new-passwd))
