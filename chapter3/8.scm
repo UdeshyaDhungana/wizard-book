517190ab5ecf3dda1cfa2e7dcf59b4a48ced3fbc
Udeshya Dhungana <075BCT095.udeshya@pcampus.edu.np>
2021-02-26 12:31:37 +0545

    started environment model

--- chapter3/8.scm
+++ chapter3/8.scm
@@ -1,2 +1,15 @@
 
 ;; Tomorrow
+
+(define f
+  (let ((expired 0))
+    (lambda (x)
+      (if (= expired 0)
+	  (set! expired (+ expired 1)) x
+	  0))))
+
+;; function expires, i.e. returns 0 after first call,
+;; returns x otherwise
+
+;; Remember that this function can be implemented in many ways,
+;; what matters is you make use of a local state variable
