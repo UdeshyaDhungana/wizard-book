5fefb0b31b435a7480f255e5a14ae54439dfcac3
Udeshya Dhungana <075BCT095.udeshya@pcampus.edu.np>
2021-03-21 11:30:04 +0545

    hare and tortoise

--- chapter3/19.scm
+++ chapter3/19.scm
@@ -3,4 +3,14 @@
 
 ;; Tortoise-Hare algorithm
 
-(define (has-cycle? x))
+;; just to make it a bit easy
+
+(define (has-cycle? x)
+  (define (helper a b first-time)
+    (cond ((or (null? b)
+	       (null? (cdr b))) false)
+	  ((or first-time
+	       (not (eq? a b)))
+	    (helper (cdr a) (cddr b) false))
+	   (else true)))
+  (helper x x true))
