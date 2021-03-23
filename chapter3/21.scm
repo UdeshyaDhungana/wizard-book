11a8376de12f716fda04d806f64e4a304aa7a3fa
Udeshya Dhungana <075BCT095.udeshya@pcampus.edu.np>
2021-03-23 19:43:38 +0545

    qyu? q? qu? It's queue.

---
+++ chapter3/21.scm
@@ -0,0 +1,12 @@
+;; Explaining the queue
+
+;; List interpreter does not know how to print a queue
+;; So it prints it as it sees.
+;; For example a queue `1, 2, 3` is actually, in our representation
+;; a pair, whose first element points to the car of the list (1 2 3)
+;; and whose cdr points to the last pair of the given list.
+;; Hence a queue `1, 2, 3` is seen on lisp evaluator as
+;; ((1 2 3) 3)
+
+;; Definition of print-queue:
+;; It is inside `queue.scm`
